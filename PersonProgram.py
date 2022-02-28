8f3edecbb08939e360474b3e0b85ec39acb709b8
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-02-28 15:42:29 -0600

    Start Inheritance

---
+++ PersonProgram.py
@@ -0,0 +1,7 @@
+import PersonClass as p
+
+def main():
+    personinfo = p.Person("Aydin","111 Way","123-456-7890")
+
+    customerinfo = p.Customer("Aydin","111 Way","123-456-7890", "123", bool(True))
+
